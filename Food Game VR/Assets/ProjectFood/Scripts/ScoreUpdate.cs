caddbcf4e6b853a2a839fd7d1ae0d30f670fe5db
Hasan <emin.oren.139@gmail.com>
2023-04-28 18:29:25 +0300

    Add scores, change drink machine images

---
+++ Food Game VR/Assets/ProjectFood/Scripts/ScoreUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour
+{
+    private GameManager manager;
+    private Text text;
+    private int localScore;
+    void Start()
+    {
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        text = GetComponent<Text>();
+        UpdateText();
+    }
+
+    void Update()
+    {
+        if (localScore != manager.Score) 
+        {
+            UpdateText();
+        }
+    }
+
+    private void UpdateText() 
+    {
+        localScore = manager.Score;
+        text.text = $"Score: {localScore}";
+    }
+}
